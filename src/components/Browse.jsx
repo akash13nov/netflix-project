35f3f7f2c1d82f89773025d040547cd465ad787c
akash13nov <akash13nov@gmail.com>
2026-08-31 17:17:03 +0530

    wip

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -5,6 +5,9 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <h1 className="text-black text-center text-8xl font-bold mt-20 ml-10">
+        Work In Progress
+      </h1>
     </div>
   );
 };
