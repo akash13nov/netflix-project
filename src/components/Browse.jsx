910b765901b016a603f278d7422fa505cbca7975
akash13nov <akash13nov@gmail.com>
2026-09-02 19:40:26 +0530

    add upcoming and top rated movies list

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -3,10 +3,14 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Container from "./Container";
 import MoviesListContainer from "./MoviesListContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
   usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
 
   return (
     <div className="relative h-screen">
--- src/components/MoviesListContainer.jsx
+++ src/components/MoviesListContainer.jsx
@@ -9,9 +9,9 @@ const MoviesListContainer = () => {
       <div className="bg-black">
         <div className="relative z-0 -mt-64">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
           <MovieList title={"Popular Movies"} movies={movies.popularMovies} />
-          <MovieList title={"Action Movies"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
           <MovieList title={"Horror Movies"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Comedy Movies"} movies={movies.nowPlayingMovies} />
         </div>
